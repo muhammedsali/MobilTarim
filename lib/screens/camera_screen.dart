355da91a6259b6a7b47e33496ee30230bd4df697
Muhammed SALİ <78242455+muhammedsali@users.noreply.github.com>
2025-05-09 12:34:12 +0300

    camera update and fix

--- lib/screens/camera_screen.dart
+++ lib/screens/camera_screen.dart
@@ -1,34 +1,50 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_mjpeg/flutter_mjpeg.dart';
 
 class CameraScreen extends StatelessWidget {
   const CameraScreen({super.key});
 
+  final String streamUrl = 'http://10.39.15.33:8888'; // Raspberry Pi IP adresi
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("Canlı Kamera Yayını")),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Text("Kamera Görüntüsü", style: TextStyle(fontSize: 18)),
-            const SizedBox(height: 16),
-            ClipRRect(
-              borderRadius: BorderRadius.circular(16),
-              child: Image.network(
-                "https://10.131.159.6:8080/",// ÖRNEK IP – okulda değiştireceksin
-                width: 300,
-                height: 200,
-                errorBuilder: (context, error, stackTrace) {
-                  return const Text(
-                    "Görüntü yüklenemedi.\nKamera bağlantısı yok.",
-                    textAlign: TextAlign.center,
-                  );
-                },
+      appBar: AppBar(
+        title: const Text('Canlı Kamera'),
+        centerTitle: true,
+        backgroundColor: Colors.black,
+      ),
+      body: Stack(
+        children: [
+          Positioned.fill(
+            child: Mjpeg(
+              stream: streamUrl,
+              isLive: true,
+              timeout: const Duration(seconds: 5),
+            ),
+          ),
+          Positioned(
+            top: 16,
+            left: 16,
+            child: Container(
+              padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 6),
+              decoration: BoxDecoration(
+                color: const Color.fromRGBO(0, 0, 0, 0.5),
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: const Row(
+                children: [
+                  Icon(Icons.videocam, color: Colors.greenAccent, size: 20),
+                  SizedBox(width: 6),
+                  Text(
+                    "Canlı Yayın Aktif",
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ],
               ),
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
